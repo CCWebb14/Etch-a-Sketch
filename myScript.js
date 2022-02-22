38793268742d4a7d63a4cec8c6bf38b6c26ddd71
Cooper Webb <CCWebb14@gmail.com>
2022-02-21 17:26:54 -0800

    reset button functionality, removing enabled class

--- myScript.js
+++ myScript.js
@@ -8,10 +8,9 @@ resetHolder.classList.add('resetHolder');
 const resetButton = document.createElement("button");
 resetHolder.appendChild(resetButton);
 resetButton.classList.add('resetButton');
-
 resetButton.innerText = "RESET";
 
-const container = document.createElement("div");
+let container = document.createElement("div");
 document.body.appendChild(container);
 container.classList.add('container');
 
@@ -19,13 +18,33 @@ let defined_size = 25;
 
 let row_size = Math.sqrt(defined_size);
 
-for(let i = 0; i<defined_size; i++){
-    if (i === 0 || (i%(row_size) === 0)){
-        currentRow = document.createElement("div");
-        container.appendChild(currentRow);
-        currentRow.classList.add('row');
+buildGrid();
+
+resetButton.addEventListener('click', resetGrid);
+
+let boxes = document.querySelectorAll('.box');
+
+boxes.forEach(box => box.addEventListener('click', enableBox));
+
+function buildGrid(){
+    
+
+    for(let i = 0; i<defined_size; i++){
+        if (i === 0 || (i%(row_size) === 0)){
+            currentRow = document.createElement("div");
+            container.appendChild(currentRow);
+            currentRow.classList.add('row');
+        }
+        newBox = document.createElement("div");
+        currentRow.appendChild(newBox);
+        newBox.classList.add('box');
     }
-    newBox = document.createElement("div");
-    currentRow.appendChild(newBox);
-    newBox.classList.add('box');
+}
+
+function enableBox(){
+    this.classList.toggle('enabled');
+}
+
+function resetGrid(){
+    boxes.forEach(box => box.classList.remove('enabled'));
 }
