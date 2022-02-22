dc13a3d2d656f14c1704e0ddd92dee0bc07e201b
Cooper Webb <CCWebb14@gmail.com>
2022-02-21 16:38:57 -0800

    flex grid that can support squared numbers, container->row->boxes

--- myScript.js
+++ myScript.js
@@ -1,12 +1,22 @@
 let newBox = 0;
+let currentRow = 0;
 
 const container = document.createElement("div");
 
 document.body.appendChild(container);
 container.classList.add('container');
 
-for(let i = 0; i<16; i++){
+let defined_size = 900;
+
+let row_size = Math.sqrt(defined_size);
+
+for(let i = 0; i<defined_size; i++){
+    if (i === 0 || (i%(row_size) === 0)){
+        currentRow = document.createElement("div");
+        container.appendChild(currentRow);
+        currentRow.classList.add('row');
+    }
     newBox = document.createElement("div");
-    container.appendChild(newBox);
+    currentRow.appendChild(newBox);
     newBox.classList.add('box');
 }
